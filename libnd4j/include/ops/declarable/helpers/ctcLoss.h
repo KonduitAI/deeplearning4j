c1fd05ccc059875603887e00d6e325a27683e813
AbdelRauf <rauf@konduit.ai>
2021-01-27 23:52:25 +0100

    licence
    Signed-off-by: AbdelRauf <rauf@konduit.ai>

--- libnd4j/include/ops/declarable/helpers/ctcLoss.h
+++ libnd4j/include/ops/declarable/helpers/ctcLoss.h
@@ -1,10 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2019 Konduit K.K.
+ * Copyright (c) 2021 Deeplearning4j Contributors
  *
  * This program and the accompanying materials are made available under the
  * terms of the Apache License, Version 2.0 which is available at
  * https://www.apache.org/licenses/LICENSE-2.0.
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -12,7 +12,7 @@
  * under the License.
  *
  * SPDX-License-Identifier: Apache-2.0
- ******************************************************************************/
+ *******************************************************************************/
 
 //
 // @author AbdelRauf
